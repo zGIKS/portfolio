c426c49f5975d34463db332286145add938cf6da
zGIKS <166082577+zGIKS@users.noreply.github.com>
2026-02-06 01:53:08 -0500

    fix: adjust padding and max-width for HomeCard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { HomeCard } from "@/components/home-card";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen justify-center bg-neutral-100 px-[clamp(2rem,1.0816rem+3.9184vw,5rem)]">
+    <main className="flex min-h-screen justify-center bg-neutral-100 px-0 md:px-[clamp(2rem,1.0816rem+3.9184vw,5rem)]">
       <HomeCard />
     </main>
   );
--- src/components/home-card.tsx
+++ src/components/home-card.tsx
@@ -1,3 +1,3 @@
 export function HomeCard() {
-  return <div className="min-h-screen w-full max-w-[80.77375rem] bg-black" />;
+  return <div className="min-h-screen w-full bg-black md:max-w-[76.7350625rem]" />;
 }
