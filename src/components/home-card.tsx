992c719c30da0637003cf71c23817c079ffe424b
zGIKS <166082577+zGIKS@users.noreply.github.com>
2026-02-06 01:57:12 -0500

    fix: update background color and add shadow to HomeCard component

--- src/components/home-card.tsx
+++ src/components/home-card.tsx
@@ -1,3 +1,3 @@
 export function HomeCard() {
-  return <div className="min-h-screen w-full bg-black md:max-w-[76.7350625rem]" />;
+  return <div className="min-h-screen w-full bg-white shadow-xl md:max-w-[76.7350625rem]" />;
 }
