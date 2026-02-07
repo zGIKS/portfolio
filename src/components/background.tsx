d69bb28c906082eb5e15efd60cda157b136f701b
zGIKS <166082577+zGIKS@users.noreply.github.com>
2026-02-06 19:38:42 -0500

    feat: add BackgroundSquare component and update Home layout with background image

---
+++ src/components/background.tsx
@@ -0,0 +1,5 @@
+export function BackgroundSquare() {
+  return (
+    <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[2000px] h-[1100px] bg-[url('/background.png')] bg-cover bg-center z-0"></div>
+  );
+}
--- src/components/home-card.tsx
+++ src/components/home-card.tsx
@@ -4,7 +4,7 @@ import { Section3 } from "./section-3";
 
 export function HomeCard() {
   return (
-    <div className="flex w-full flex-col bg-black text-white shadow-xl md:max-w-[55.249245rem]">
+    <div className="relative z-10 flex w-full flex-col bg-black text-white shadow-xl md:max-w-[55.249245rem]">
       <Section1 />
       <div className="flex w-full flex-1 flex-col-reverse md:flex-row">
         <Section2 />
