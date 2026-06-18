9ce6fec6a1f6d83fa82cb0ac00713e04d37ed4e0
zGIKS <166082577+zGIKS@users.noreply.github.com>
2026-06-17 23:19:49 -0500

    refactor: remove BlogContent component from HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Suspense } from "react";
 import { Workspace } from "@/contexts/blog/interfaces/components/workspace";
 import { Header } from "@/contexts/blog/interfaces/components/header";
-import { BlogContent } from "@/contexts/blog/interfaces/components/blog-content";
 
 type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 
@@ -14,7 +13,6 @@ export default async function HomePage({ searchParams }: { searchParams: SearchP
           Cargando contenido...
         </div>
       }>
-        <BlogContent searchParams={searchParams} />
       </Suspense>
     </Workspace>
   );
